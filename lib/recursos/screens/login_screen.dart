648c9e307a48b0817acbfb970e6e2b21b5e9927c
reynerlalacrita <reyneralejand@gmail.com>
2026-06-13 13:31:54 -0400

    esto

--- lib/recursos/screens/login_screen.dart
+++ lib/recursos/screens/login_screen.dart
@@ -165,7 +165,7 @@ class LoginScreen extends StatelessWidget {
                     // Texto Forgot Password?
                     
                     TextButton(
-                      onPressed: () { a a a a a
+                      onPressed: () {
                         // Acción de recuperar contraseña
                       },
                       child: const Text(
